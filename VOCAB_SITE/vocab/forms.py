1fd037bfe5893bd516e4b5a41662ab737aa619c6
Andy Creighton <cr8onski@gmail.com>
2016-03-04 09:31:15 -0500

    added basic forms

--- VOCAB_SITE/vocab/forms.py
+++ VOCAB_SITE/vocab/forms.py
@@ -2,3 +2,24 @@ from django import forms
 
 class LoginForm(forms.Form):
     userName = forms.CharField(label='User name', max_length=10)
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(label='Subject', max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
+
+class TermForm(forms.Form):
+    domain = forms.CharField(label='Domain:', max_length=40)
+    resourceType = forms.CharField(label='Resource Type:', max_length=10)
+    vocabulary = forms.CharField(label='Vocabulary/Profile', max_length=40)
+    termType = forms.ChoiceField(label='Term Type', initial='Activity Type', choices=(('verb', 'Verb'), ('activityType', 'Activity Type')))
+    term = forms.CharField(label='Term', max_length=40)
+
+class VocabForm(forms.Form):
+    # this is quite repetitive at the moment, will simplify later
+    domain = forms.CharField(label='Domain:', max_length=40)
+    resourceType = forms.CharField(label='Resource Type:', max_length=10)
+    vocabulary = forms.CharField(label='Vocabulary/Profile', max_length=40)
+    termType = forms.CharField(label='Term Type', max_length=40)
+    term = forms.CharField(label='Term', max_length=40)
--- VOCAB_SITE/vocab/views.py
+++ VOCAB_SITE/vocab/views.py
@@ -1,24 +1,96 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponseRedirect, HttpResponse
-from .forms import LoginForm
-
+from .forms import LoginForm, ContactForm, TermForm, VocabForm
+from django.core.mail import send_mail
 
 @require_http_methods(["GET"])
 def home(request):
     return render(request, 'home.html')
 
-@require_http_methods(["GET"])
+@require_http_methods(["GET", "POST"])
 def createURI(request):
-    return render(request, 'createURI.html')
+    # return render(request, 'login.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = TermForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            domain = form.cleaned_data['domain']
+            resourceType = form.cleaned_data['resourceType']
+            vocabulary = form.cleaned_data['vocabulary']
+            termType = form.cleaned_data['termType']
+            term = form.cleaned_data['term']
+            newuri = 'https://' + domain + '/' + resourceType + '/' + vocabulary + '/' + termType + '/' + term
+            # redirect to a new URL:
+            return HttpResponse('you did not create a term, but we did trick this info out of you: <br> %s' % newuri)
 
-@require_http_methods(["GET"])
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TermForm()
+
+    return render(request, 'createURI.html', {'form': form})
+
+@require_http_methods(["GET", "POST"])
 def createUser(request):
-    return render(request, 'createUser.html')
+    # return render(request, 'createUser.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            # return HttpResponse('baby step get on the elevator <br> %s' % form.cleaned_data['subject'])
+            return HttpResponseRedirect('/')
+            #above are two options to respond simply
+            #below is an example from documentation, let's see what happens
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
 
-@require_http_methods(["GET"])
+            recipients = ['andrew.creighton.ctr@adlnet.gov']
+            if cc_myself:
+                recipients.append(sender)
+
+            send_mail(subject, message, sender, recipients)
+            return HttpResponseRedirect('/')
+            #this does not send and email, but since that's not what we really want here anyway - i think we're alright
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm()
+
+    return render(request, 'createUser.html', {'form': form})
+
+@require_http_methods(["GET", "POST"])
 def createVocab(request):
-    return render(request, 'createVocab.html')
+    # return render(request, 'login.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = VocabForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            domain = form.cleaned_data['domain']
+            resourceType = form.cleaned_data['resourceType']
+            vocabulary = form.cleaned_data['vocabulary']
+            termType = form.cleaned_data['termType']
+            term = form.cleaned_data['term']
+            # redirect to a new URL:
+            return HttpResponse("that's a lot of words are you finished now? <br> %s" % vocabulary)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = VocabForm()
+
+    return render(request, 'createVocab.html', {'form': form})
 
 @require_http_methods(["GET"])
 def userProfile(request):
